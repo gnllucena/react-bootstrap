140696b6f5052f2d1597fb054c3f2ac5ff66480b
Gabriel Lucena <gnllucena@gmail.com>
2020-07-29 00:38:13 -0300

    Sass

---
+++ src/routes/home/HomePage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export const HomePage: React.FC = () => {
+  React.useEffect(() => {
+    return () => { };
+  }, []);
+
+  return (
+    <div>
+      <p>Home Page</p>
+    </div>
+  );
+}
---
+++ src/routes/stock/StockPage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export const StockPage: React.FC = () => {
+  React.useEffect(() => {
+    return () => { };
+  }, []);
+
+  return (
+    <div>
+      <p>Stock Page</p>
+    </div>
+  );
+}
